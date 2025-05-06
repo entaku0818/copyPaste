69eed122325d3e58fda0413d9d11b3383f33adb7
entaku <entaku19890818@gmail.com>
2025-05-06 22:16:40 +0900

    refactor: Simplify ContentView initialization by removing redundant init method

--- copyPaste/ContentView.swift
+++ copyPaste/ContentView.swift
@@ -4,10 +4,6 @@ import ComposableArchitecture
 public struct ContentView: View {
     let store: StoreOf<ClipboardHistoryFeature>
     
-    public init(store: StoreOf<ClipboardHistoryFeature>) {
-        self.store = store
-    }
-    
     public var body: some View {
         NavigationStack {
             ClipboardHistoryView(store: store)
